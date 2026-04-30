356c9ee7b8127542480606ebf018ea0d19983998
Udayraj Saroj <udayrajsaroj55@gmail.com>
2026-04-30 19:54:19 +0530

    fav icon changed

--- client/src/pages/GameBackground.js
+++ client/src/pages/GameBackground.js
@@ -1,36 +1,8 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import './GameBackground.css';
 
-const items = ['🎁', '👕', '📦', '🍎', '🧸', '💖'];
-
 const GameBackground = () => {
-    const gameItems = useMemo(() => 
-        [...Array(8)].map((_, i) => ({  // 12 → 8
-            id: i,
-            left: `${(i * 11) % 90}%`,  // Math.random() hataya
-            delay: `${(i * 1.1) % 8}s`,
-            duration: `${6 + (i % 4)}s`
-        })), []  // sirf ek baar calculate hoga
-    );
-
-    return (
-        <div className="game-container">
-            <div className="grid-floor"></div>
-            {gameItems.map((item) => (
-                <div
-                    key={item.id}
-                    className="game-item"
-                    style={{
-                        left: item.left,
-                        animationDelay: item.delay,
-                        animationDuration: item.duration
-                    }}
-                >
-                    {items[item.id % items.length]}
-                </div>
-            ))}
-        </div>
-    );
+    return <div className="game-container"></div>;
 };
 
 export default GameBackground;
